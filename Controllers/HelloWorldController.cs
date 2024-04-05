32fbb165304f9cc027d98129e4d0510486dc4f8f
chasejenkins303 <chasejenkins303@gmail.com>
2024-04-04 22:53:45 -0400

    initial commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+public class HelloWorldController:Controller
+{
+    //GET: /HelloWorld/
+    public IActionResult Index(){
+        return View();
+    }
+    
+    //Get: HelloWorld/Welcome
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NEW_ASP.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
